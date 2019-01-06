df95fef74d7262b72a5d6ca03faee22823184845
ONE-BIT <pclub.onebit@gmail.com>
2019-01-06 20:26:28 +0900

    Specified key was too long対策のため、マイグレーションでできる文字列型のデフォルト桁数を変える

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
+        Schema::defaultStringLength(191);
     }
 
     /**
